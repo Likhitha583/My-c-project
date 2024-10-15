5745f5a7d41d8bd867ae93fe25dfc31d770da0ee
Likhitha583 <blikhithavenkatasaisri@gmail.com>
2024-10-15 16:16:08 +0530

    Update greatest number.c

--- greatest number.c
+++ greatest number.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 main(){
 int a,b,c,d;
+printf(enter the values of a,b,c,d);
 scanf("%d %d %d %d", &a,&b, &c, &d);
 if(a>b)
-if(a>c)
-if(a>d)
-printf("a is big"); else printf("d is big");
+  if(a>c)
+    if(a>d)
+      printf("a is big"); 
+    else 
+     printf("d is big");
+  else
+   if (c>d) 
+    printf("c"); 
+   else 
+    printf("d");
 else
-if (c>d) printf("c"); else printf("d");
-else
-if(b>c)
-if(b>d)
-printf("b");
-else
-printf("d");
-else if(c>d) printf("c");
-else
-printf("d");
+  if(b>c)
+    if(b>d)
+     printf("b");
+    else
+     printf("d");
+   else
+     if(c>d)
+      printf("c");
+     else
+      printf("d");
 }
